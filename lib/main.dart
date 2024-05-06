d5cd2ebfada811b2195a549d1ce5156b3b6d5d54
Wan Muzaffar <wanmuz86@gmail.com>
2024-05-06 12:24:06 +0800

    add ScrollView (SingleChildScrollView)

--- lib/main.dart
+++ lib/main.dart
@@ -35,46 +35,48 @@ class HomePage extends StatelessWidget {
         title: Text("Hello World"),
         backgroundColor: Colors.green,
       ),
-      body: Center(
-        child: Column(
-          children: [
-            Text(
-              "Hello world",
-              style: TextStyle(fontSize: 32, color: Colors.red),
-            ),
-            SizedBox(height: 8,),
-            Text(
-              "Welcome to my app",
-              style: TextStyle(fontSize: 24, color: Colors.cyanAccent),
-            ),
-            SizedBox(height: 8,),
-            Text(
-              "I hope you enjoy it",
-              style: TextStyle(fontSize: 20, color: Colors.deepPurple),
-            ),
-            SizedBox(height: 8,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("This is left", style: TextStyle(fontSize: 20, color: Colors.deepPurple),),
-                SizedBox(width: 16,),
-                Text("This is right", style: TextStyle(fontSize: 20, color: Colors.deepPurple),)
-              ],
-            ),
-            SizedBox(height: 8,),
-            Image.network("https://a.travel-assets.com/findyours-php/viewfinder/images/res70/473000/473015-Kuala-Lumpur.jpg"),
-            SizedBox(height: 8,),
-            TextField(
-              controller: nameEditingController,
-              decoration:
-              InputDecoration(hintText: "Enter your name"),),
-            TextButton(
-              onPressed: (){
-                print("Hello ${nameEditingController.text}");
-              },
-              child: Text("Press me!"),
-            )
-          ],
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              Text(
+                "Hello world",
+                style: TextStyle(fontSize: 32, color: Colors.red),
+              ),
+              SizedBox(height: 8,),
+              Text(
+                "Welcome to my app",
+                style: TextStyle(fontSize: 24, color: Colors.cyanAccent),
+              ),
+              SizedBox(height: 8,),
+              Text(
+                "I hope you enjoy it",
+                style: TextStyle(fontSize: 20, color: Colors.deepPurple),
+              ),
+              SizedBox(height: 8,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("This is left", style: TextStyle(fontSize: 20, color: Colors.deepPurple),),
+                  SizedBox(width: 16,),
+                  Text("This is right", style: TextStyle(fontSize: 20, color: Colors.deepPurple),)
+                ],
+              ),
+              SizedBox(height: 8,),
+              Image.network("https://a.travel-assets.com/findyours-php/viewfinder/images/res70/473000/473015-Kuala-Lumpur.jpg"),
+              SizedBox(height: 8,),
+              TextField(
+                controller: nameEditingController,
+                decoration:
+                InputDecoration(hintText: "Enter your name"),),
+              TextButton(
+                onPressed: (){
+                  print("Hello ${nameEditingController.text}");
+                },
+                child: Text("Press me!"),
+              )
+            ],
+          ),
         ),
       ),
     );
