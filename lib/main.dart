5af901b5dc00ae03ae78d5d47b014d88b28ba1a9
Wan Muzaffar <wanmuz86@gmail.com>
2024-05-06 14:51:13 +0800

    add padding and change to const

--- lib/main.dart
+++ lib/main.dart
@@ -33,61 +33,64 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.yellow,
       appBar: AppBar(
-        title: Text("Hello World"),
+        title: const Text("Hello World"),
         backgroundColor: Colors.green,
       ),
       body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            children: [
-              Text(
-                "Hello world",
-                style: GoogleFonts.quicksand(
-                  textStyle:TextStyle(fontSize: 32, color: Colors.red)
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Center(
+            child: Column(
+              children: [
+                Text(
+                  "Hello world",
+                  style: GoogleFonts.quicksand(
+                    textStyle:const TextStyle(fontSize: 32, color: Colors.red)
+                  ),
                 ),
-              ),
-              SizedBox(height: 8,),
-              Text(
-                "Welcome to my app",
-                // THe font is camelCase whenever there is a space in font name in Google Font web
-                // It will become capital letter here
-                style: GoogleFonts.bebasNeue(
-                  textStyle:TextStyle(fontSize: 24, color: Colors.cyanAccent)
+                const SizedBox(height: 8,),
+                Text(
+                  "Welcome to my app",
+                  // THe font is camelCase whenever there is a space in font name in Google Font web
+                  // It will become capital letter here
+                  style: GoogleFonts.bebasNeue(
+                    textStyle:const TextStyle(fontSize: 24, color: Colors.cyanAccent)
+                  ),
                 ),
-              ),
-              SizedBox(height: 8,),
-              Text(
-                "I hope you enjoy it",
-                style: TextStyle(fontSize: 20, color: Colors.deepPurple),
-              ),
-              SizedBox(height: 8,),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("This is left", style: TextStyle(fontSize: 20, color: Colors.deepPurple),),
-                  SizedBox(width: 16,),
-                  Text("This is right", style: TextStyle(fontSize: 20, color: Colors.deepPurple),)
-                ],
-              ),
-              SizedBox(height: 8,),
-              Image.network("https://a.travel-assets.com/findyours-php/viewfinder/images/res70/473000/473015-Kuala-Lumpur.jpg"),
-              SizedBox(height: 8,),
-              Image.asset("images/app_poster.png"),
-              SizedBox(height: 8,),
-              TextField(
-                controller: nameEditingController,
-                decoration:
-                InputDecoration(hintText: "Enter your name"),),
-              TextButton(
-                onPressed: (){
-                  var snackBar = SnackBar(content: Text("Hello ${nameEditingController.text}"));
-                 ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                const SizedBox(height: 8,),
+                const Text(
+                  "I hope you enjoy it",
+                  style: TextStyle(fontSize: 20, color: Colors.deepPurple),
+                ),
+                const SizedBox(height: 8,),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("This is left", style: TextStyle(fontSize: 20, color: Colors.deepPurple),),
+                    SizedBox(width: 16,),
+                    Text("This is right", style: TextStyle(fontSize: 20, color: Colors.deepPurple),)
+                  ],
+                ),
+                const SizedBox(height: 8,),
+                Image.network("https://a.travel-assets.com/findyours-php/viewfinder/images/res70/473000/473015-Kuala-Lumpur.jpg"),
+                const SizedBox(height: 8,),
+                Image.asset("images/app_poster.png"),
+                const SizedBox(height: 8,),
+                TextField(
+                  controller: nameEditingController,
+                  decoration:
+                  const InputDecoration(hintText: "Enter your name"),),
+                TextButton(
+                  onPressed: (){
+                    var snackBar = SnackBar(content: Text("Hello ${nameEditingController.text}"));
+                   ScaffoldMessenger.of(context).showSnackBar(snackBar);
 
-                  // print("Hello ${nameEditingController.text}");
-                },
-                child: Text("Press me!"),
-              )
-            ],
+                    // print("Hello ${nameEditingController.text}");
+                  },
+                  child: const Text("Press me!"),
+                )
+              ],
+            ),
           ),
         ),
       ),
